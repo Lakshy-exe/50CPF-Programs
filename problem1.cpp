007c23a6587a2a6a60c44fb35f62f052932fe4f2
Lakshy-exe <lakshya7758@gmail.com>
2026-07-26 22:36:46 +0530

    first problem solved

--- problem1.cpp
+++ problem1.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::endl;
 int main()
 {
-    cout<<"This is just a demo code.";
+    int GpWins = 71;
+    float Career_points = 3535.5;
+    double pi = 3.141592653589793;
+    char Driver = 'V';
+    bool isTrue = true;
+
+    cout<<"This is an integer data type: "<<GpWins<<endl;
+    cout<<"This is a float data type: "<<Career_points<<endl;
+    cout<<"This is a double data typr: "<<pi<<endl;
+    cout<<"This is a character data type: "<<Driver<<endl;
+    cout<<"This is a bool data type: "<<isTrue<<endl;
     return 0;
 }
