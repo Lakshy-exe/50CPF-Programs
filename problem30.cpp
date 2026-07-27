b24fc1500910c5b81d61f5d3106d9532b2bef456
Lakshy-exe <lakshya7758@gmail.com>
2026-07-28 00:35:08 +0530

    Problem 30

---
+++ problem30.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter first value (0 or 1): ";
+    cin >> a;
+
+    cout << "Enter second value (0 or 1): ";
+    cin >> b;
+
+    cout << "a && b = " << (a && b) << endl;
+    cout << "a || b = " << (a || b) << endl;
+    cout << "!a = " << (!a) << endl;
+    cout << "!b = " << (!b) << endl;
+
+    return 0;
+}
