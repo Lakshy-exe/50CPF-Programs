af98acfb245fea77e9362ae20427226dc0e02ddf
Lakshy-exe <lakshya7758@gmail.com>
2026-07-27 22:08:28 +0530

    This is the code for problem 5

---
+++ problem5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout<<"Program to add two numbers of user's input";
+    cout<<"Enter first number::";
+    cin>>a;
+    cout<<"\nEnter second number::";
+    cin>>b;
+    sum = a + b;
+    cout<<"\nThe sum of the numbers is::"<<sum;
+    return 0;
+}
