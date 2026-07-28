26ff7534a51b8ad2e808accc409fd29f11360e29
Lakshy-exe <lakshya7758@gmail.com>
2026-07-28 07:35:54 +0530

    Problem37

---
+++ problem37.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+
+    x += 5;
+    cout << "After += 5: " << x << endl;
+
+    x -= 3;
+    cout << "After -= 3: " << x << endl;
+
+    x *= 2;
+    cout << "After *= 2: " << x << endl;
+
+    x /= 4;
+    cout << "After /= 4: " << x << endl;
+
+    x %= 3;
+    cout << "After %= 3: " << x << endl;
+
+    return 0;
+}
